8a1b34f9e5d7ace8a3b0430a23b569f1fdced3c3
Stephen MacNeil <smacneil01@gmail.com>
2014-08-28 20:06:00 -0400

    add project configurations

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject experiment "0.1.0-SNAPSHOT"
+  :description "Testing out clojure with some easy problems"
+  :url "http://github.com/stevemacn"
+  :license {:name "MIT License"
+            :url "http://mit.edu"}
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+  ]
+  :dev-dependencies [
+    [no-man-is-an-island/lein-eclipse "2.0.0"]          
+  ]
+  :main experiment.core
+)
