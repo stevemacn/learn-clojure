ee1db2a369d194d6fc7d0a373dc963d6029f3814
Stephen MacNeil <smacneil01@gmail.com>
2014-09-15 21:09:00 -0400

    add lisp factorial and first try at missing-element

--- src/experiment/core.clj
+++ src/experiment/core.clj
@@ -1,7 +1,9 @@
-(ns experiment.core)
+(ns experiment.core
+  (:require [clojure.math.numeric-tower :as math])
+)
 
 ; author: stephen macneil
-; problem:
+; problem: find the missing element in a set of numbers
 
 
 (defn -main [& args]
@@ -10,7 +12,7 @@
 ;trying to learn recursion
 ;calculate b^e recursively
 (defn power [b, e]
-  (println b e)
+  ;(println b e)
 
   (cond
     (= e 0) 1
@@ -22,7 +24,7 @@
 
 (defn power2 [c, a]
   (loop [b c e a]
-    (println b e)
+    ;(println b e)
     (cond
       (= e 0) 1
       (= e 1) b
@@ -31,12 +33,8 @@
   )
 )
 
-
 (println "tail recursion (exponent):" (power 2 5))
 (println "real tail recur (exponent):" (power2 2 5))
-
-
-
 
 (defn factorial [x]
   (loop [n x f 1]
@@ -47,3 +45,34 @@
   )
 )
 (println (factorial 5))
+
+
+;for practice (course) lisp tail recursion (wouldn't be optimized in clojure)
+(defn fact-aux [n a]
+  (if (= n 1)
+      a
+      (fact-aux (dec n) (* n a))
+  )
+)
+; must occur after fast-fact-aux otherwise it won't find the fx
+(defn fact [x]
+  (fact-aux x 1)
+)
+
+(println (fact 5))
+
+;map-reduce version (not working)
+;(println "map reduce factorial:" (reduce *' (range 1 5)) )
+
+;probably the easiest way to find the missing element
+(require 'clojure.set )
+
+(println
+  ;union - join of the two sets
+  (clojure.set/difference
+    ;cast both lists to sets (safe for duplicate values)
+    (into #{} [4 1 0 2 9 6 9 7 5 3 14])
+    (into #{} [4 6 7 2 1 1 11 0 0 8 3 9])
+    )
+  )
+
