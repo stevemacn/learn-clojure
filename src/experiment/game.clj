c3aa6094bd0ce7df059b2251d5b4c1144db1e970
Stephen MacNeil <smacneil01@gmail.com>
2014-10-23 19:17:27 -0400

    user can submit their usernames, turn based game-loop

--- src/experiment/game.clj
+++ src/experiment/game.clj
@@ -1,5 +1,6 @@
 (ns experiment.game)
 
+;======Character utilities====
 (defn get-characters []
   #{"Le Chiffre" "Dr. Julius No" "Colonel von Hammerstein" "General Ourumov" "Roger Moore" "Sean Connery"  "OddJob"})
 
@@ -11,6 +12,10 @@
 (defn print-characters [xs]
   (apply println xs))
 
+;Remove item: returns a list with the specified item removed
+(defn remove-character [x xs]
+  (remove #{x} xs ))
+
 ;Count Occurences: count the number of villains that are present in the list of suspects
 (defn count-occurences [xs ys]
   (if (empty? xs)
@@ -19,38 +24,65 @@
       (if (contains? (set ys) (first xs)) 1 0)
       (count-occurences (rest xs) ys))))
 
+;======Recursive game loop====
+
+(defn print-game-state [x y user]
+  (println "The characters are" y)
+  (println (count-occurences x y) "are villains!")
+  (println (first user) "- who is the double-agent?")
+)
+
+
 ;Game Loop: keeps the villains the same and updates the suspects to display a different number each time.
-(defn game-loop [villains z]
+(defn game-loop [villains z, users]
 
-  (loop [x villains y (get-suspects 5) z z]
-
+  (loop [x villains y (get-suspects 5) z z, users users]
 
     (println x)
-    (println "The characters are" y)
-    (println (count-occurences x y) "are villains!")
+    (print-game-state x y users)
 
-    ;(println "villains are" x)
-    (println "username," "who is the double-agent?")
     ;take command from user and check to see if they win or loose
     (let [line (read-line)]
       (if (not (= line "skip"))
-        (if (contains? (set x) line) (println "win") (println "lose"))
-        (if (= z 0) 0 (recur x (get-suspects 5) (dec z) ))
+        (if (contains? (set x) line)
+          (println "win")                                   ;remove villain from group.
+          (println "lose"))                                 ;remove player from game.
+        (if (= z 0)
+          0
+          (recur x (get-suspects 5) (dec z) (cons (last users) (butlast users)) ))
       )
     )
 
   )
 )
 
+;======Initialize the game====
+; Run: set up the number of players and start the game loop.
 (defn run [x]
   ;<- here we allow people to start playing register
   ;the number of players and pass that to the game loop
-  (game-loop (get-suspects 3) 4))
+
+  (println "Please type the name for each player (press only enter to start playing)")
+
+  (loop [x '()]
+    (println x)
+    (let [line (read-line)]
+      (if (= line "")
+        (game-loop (get-suspects 3) 4 x))
+        (recur (concat x (list line)))
+    )
+  )
+)
+
 
 
 ;open questions
 ;
 ; how do we break out of the game loop on game end (win or lose)?
+; all players gone: "you all lose"
+; all villains gone: congrats to player guessing - you win
+
+
 
 
 
