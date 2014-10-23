06b4571cb2b7151f5c2132594ed5d178231da93b
Stephen MacNeil <smacneil01@gmail.com>
2014-10-23 17:41:45 -0400

    added a recursive game loop and grading to find number of villains presented

--- src/experiment/game.clj
+++ src/experiment/game.clj
@@ -1,32 +1,37 @@
 (ns experiment.game)
 
 (defn get-characters []
-  '("Le Chiffre" "Dr. Julius No" "Colonel von Hammerstein" "General Ourumov" "Roger Moore" "Sean Connery"  "OddJob" )
-)
+  #{"Le Chiffre" "Dr. Julius No" "Colonel von Hammerstein" "General Ourumov" "Roger Moore" "Sean Connery"  "OddJob"})
 
 ;called once to set the villians
 ;set a hash for these characters or
 ;return tuples (selected/not selected)
-(defn get-villains [x]
-  (take x (shuffle (get-characters)))
-)
-
 (defn get-suspects [x]
-  (take x (shuffle (get-characters)))
-)
+  (take x (shuffle (get-characters))))
 
 ;doesn't quite work - print line by line
 (defn print-characters [xs]
-  (apply println xs)
-)
+  (apply println xs))
 
+(defn count-occurences [xs ys]
+  (if (empty? xs)
+    0
+    (+
+      (if (contains? (set ys) (first xs)) 1 0)
+      (count-occurences (rest xs) ys))))
+
+(defn game-loop [villains]
+
+  (loop [x villains y (get-suspects 5)]
+
+    (println "characters are" y)
+    (println "villains are" x)
+
+    (println (count-occurences x y))
+))
 
 (defn run [x]
-
-  (print-characters (get-villains 3) )
-  (println (get-villains 3) )
-
-)
+  (game-loop (get-suspects 3)))
 
 
 
