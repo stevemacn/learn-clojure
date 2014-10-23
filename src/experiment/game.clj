bcf9363079b0de27d039c54d9bb02d0a11d948d6
Stephen MacNeil <smacneil01@gmail.com>
2014-10-23 18:09:57 -0400

    add comments to explain functionality

--- src/experiment/game.clj
+++ src/experiment/game.clj
@@ -3,16 +3,15 @@
 (defn get-characters []
   #{"Le Chiffre" "Dr. Julius No" "Colonel von Hammerstein" "General Ourumov" "Roger Moore" "Sean Connery"  "OddJob"})
 
-;called once to set the villians
-;set a hash for these characters or
-;return tuples (selected/not selected)
+;Get Suspects: get x random characters from the pool of characters.
 (defn get-suspects [x]
   (take x (shuffle (get-characters))))
 
-;doesn't quite work - print line by line
+;doesn't quite work - print each item line by line
 (defn print-characters [xs]
   (apply println xs))
 
+;Count Occurences: count the number of villains that are present in the list of suspects
 (defn count-occurences [xs ys]
   (if (empty? xs)
     0
@@ -20,6 +19,7 @@
       (if (contains? (set ys) (first xs)) 1 0)
       (count-occurences (rest xs) ys))))
 
+;Game Loop: keeps the villains the same and updates the suspects to display a different number each time.
 (defn game-loop [villains z]
 
   (loop [x villains y (get-suspects 5) z z]
@@ -34,7 +34,15 @@
 )
 
 (defn run [x]
+  ;<- here we allow people to start playing register
+  ;the number of players and pass that to the game loop
   (game-loop (get-suspects 3) 4))
+
+
+;open questions
+;
+; how do we break out of the game loop on game end (win or lose)?
+
 
 
 
